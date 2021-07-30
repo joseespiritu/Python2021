6491b26657e001afe0d16c9fcac7fbf8d85d7da0
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-29 21:39:41 -0500

    Variables y direcciones de memoria

---
+++ Leccion01/main.py
@@ -0,0 +1,24 @@
+print("Hola Mundo")
+
+# Variables
+miVariable = "Hola Mundo desde Python"
+miVariable2 = 5
+print(miVariable)
+print(miVariable2)
+
+miVariable2 = 25
+print(miVariable2)
+
+# Más Variables
+x = 10
+y = 2
+z = x + y
+
+print(x)
+print(y)
+print(z)
+
+# Direcciones de memoria -> Referencias de memoria
+print(id(x))
+print(id(y))
+print(id(z))

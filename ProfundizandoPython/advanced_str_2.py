775e603cebac2b7beab79e7fdc9e55ad94c9c0d7
joseespiritu <joseespiritu89@aragon.unam.mx>
2025-03-16 21:21:29 -0600

    advanced Strings

---
+++ ProfundizandoPython/advanced_str_2.py
@@ -0,0 +1,26 @@
+# # Multiplicando str
+# resultado = 5*'Hola'
+# print(f'Resultado: {resultado}')
+#
+# # Multiplicación tuplas
+# resultado = 5*('Hola', 10)
+# print(f'Resultado: {resultado}')
+#
+# # Multiplicación Listas
+# resultado = 10*[0]
+# print(f'Resultado: {resultado}, largo: {len(resultado)}')
+
+# Caracteres de escape
+resultado = 'Hola \' Mundo'
+print(f'Resultado: {resultado}')
+
+resultado = 'Se va a eliminar el punto.\b\b\b'
+print(f'Resultado: {resultado}')
+
+# Caracter \
+resultado = 'c:\\directorio\\jose'
+print(f'Resultado: {resultado}')
+
+# raw string
+resultado = r'Cadena con \n salto de linea'
+print(f'Resultado: {resultado}')
